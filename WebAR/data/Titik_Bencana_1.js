09be39053c0c35bebccaccbafb1f794d82673536
Yoan Ayub Rizky Permadi <yoanayub123@gmail.com>
2022-07-05 03:26:26 +0700

    first commit

---
+++ WebAR/data/Titik_Bencana_1.js
@@ -0,0 +1,15 @@
+var json_Titik_Bencana_1 = {
+"type": "FeatureCollection",
+"name": "Titik_Bencana_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nama": "GT1", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.411041652241877, -7.238763237270189 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nama": "GT2", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.396995552535714, -7.224418284378791 ] } },
+{ "type": "Feature", "properties": { "id": "3", "nama": "GT3", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.371891884975767, -7.23039534808354 ] } },
+{ "type": "Feature", "properties": { "id": "4", "nama": "GT4", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.396397846165243, -7.254901309273015 ] } },
+{ "type": "Feature", "properties": { "id": "5", "nama": "GT5", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.40117949712905, -7.312281120838614 ] } },
+{ "type": "Feature", "properties": { "id": "6", "nama": "GT6", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.452582244989898, -7.24713112645684 ] } },
+{ "type": "Feature", "properties": { "id": "7", "nama": "GT7", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.373685004087193, -7.26984396853489 ] } },
+{ "type": "Feature", "properties": { "id": "8", "nama": "GT8", "jenis": "Gerakan Tanah" }, "geometry": { "type": "Point", "coordinates": [ 109.388029956978599, -7.327821486470964 ] } }
+]
+}
---
+++ WebGT/data/Titik_Bencana_1.js
@@ -0,0 +1,9 @@
+var json_Titik_Bencana_1 = {
+"type": "FeatureCollection",
+"name": "Titik_Bencana_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nama": "GT1", "jenis": "Gerakan Tanah", "tempat": "Sirau", "dampak": "1 rumah rusak" }, "geometry": { "type": "Point", "coordinates": [ 109.396995552535714, -7.224418284378791 ] } },
+{ "type": "Feature", "properties": { "id": "2", "nama": "GT2", "jenis": "Gerakan Tanah", "tempat": "Banjarsari", "dampak": "2 rumah rusak" }, "geometry": { "type": "Point", "coordinates": [ 109.373685004087193, -7.26984396853489 ] } }
+]
+}
